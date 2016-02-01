afbc2925beea4fa2a176f09c76776b4d92665bf0
Alystrasz <raes.remy@gmail.com>
2016-02-01 11:52:18 +0100

    Modification du script git

--- git.sh
+++ git.sh
@@ -13,9 +13,23 @@ read choix
 #utiliser le select bash
 if test $choix = 1
 then
+	clear
+	echo Projet HulaHoop - Interface GitHub
+	echo ---------------------------------------
+	echo 1- Pousser une modification sur le serveur
+	echo 2- Récupérer le dernier code depuis le serveur
+	echo 3- Quitter
+	echo
 	echo Entrez le nom du commit:
 	read commit
 	echo
+	echo Projet HulaHoop - Interface GitHub
+	echo ---------------------------------------
+	echo 1- Pousser une modification sur le serveur
+	echo 2- Récupérer le dernier code depuis le serveur
+	echo 3- Quitter
+	echo
+
 	git commit -am "$commit"
 	echo
 	git push origin master
@@ -23,7 +37,13 @@ then
 else
 	if test $choix = 2
 	then
-		echo 
+		clear
+		echo Projet HulaHoop - Interface GitHub
+		echo ---------------------------------------
+		echo 1- Pousser une modification sur le serveur
+		echo 2- Récupérer le dernier code depuis le serveur
+		echo 3- Quitter
+		echo
 		git pull
 		echo
 	else
